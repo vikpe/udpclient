4a69f86db1485a6f70388c6028caf002d159def2
Viktor Persson <viktor.persson@arcsin.se>
2022-05-06 22:34:58 +0200

    Add tests.

--- udpclient_test.go
+++ udpclient_test.go
@@ -1,10 +1,84 @@
 package udpclient_test
 
 import (
+	"errors"
 	"fmt"
+	"net"
+	"testing"
+	"time"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/vikpe/udpclient"
 )
+
+func captureUdpPacket(addr string) {
+	conn, _ := net.ListenPacket("udp", addr)
+	buffer := make([]byte, 1024)
+	messageLength, dst, _ := conn.ReadFrom(buffer)
+	message := buffer[:messageLength]
+	response := "OK: " + string(message)
+	conn.WriteTo([]byte(response), dst)
+}
+
+func TestClient_SendPacket(t *testing.T) {
+	addr := ":8000"
+
+	go func() {
+		captureUdpPacket(addr)
+	}()
+
+	time.Sleep(10 * time.Millisecond)
+
+	client := udpclient.New()
+	response, err := client.SendPacket(addr, []byte("PING"))
+
+	assert.Equal(t, "OK: PING", string(response))
+	assert.Equal(t, nil, err)
+}
+
+func TestClient_SendCommand(t *testing.T) {
+	testCases := []struct {
+		port                 int
+		command              udpclient.Command
+		expectedResponseBody []byte
+		expectedError        error
+	}{
+		{
+			9000,
+			udpclient.Command{
+				RequestPacket:  []byte("HELLO WORLD"),
+				ResponseHeader: []byte("OK: "),
+			},
+			[]byte("HELLO WORLD"),
+			nil,
+		},
+		{
+			9001,
+			udpclient.Command{
+				RequestPacket:  []byte("HELLO WORLD"),
+				ResponseHeader: []byte("NOT OK: "),
+			},
+			nil,
+			errors.New(":9001: Invalid response header"),
+		},
+	}
+
+	for _, tc := range testCases {
+		udpAddress := fmt.Sprintf(":%d", tc.port)
+
+		go func() {
+			captureUdpPacket(udpAddress)
+		}()
+
+		time.Sleep(10 * time.Millisecond)
+
+		client := udpclient.New()
+		response, err := client.SendCommand(udpAddress, tc.command)
+
+		assert.Equal(t, tc.expectedResponseBody, response)
+		assert.Equal(t, tc.expectedError, err)
+	}
+}
 
 func ExampleNewWithConfig() {
 	config := udpclient.Config{
