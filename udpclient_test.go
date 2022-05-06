fc15e1f43b87eb92625c2554f0ea5aee89f5514f
Viktor Persson <viktor.persson@arcsin.se>
2022-05-06 23:17:51 +0200

    Update tests.

--- udpclient_test.go
+++ udpclient_test.go
@@ -11,7 +11,7 @@ import (
 	"github.com/vikpe/udpclient"
 )
 
-func captureUdpPacket(addr string) {
+func udpListenAndEcho(addr string) {
 	conn, _ := net.ListenPacket("udp", addr)
 	buffer := make([]byte, 1024)
 	messageLength, dst, _ := conn.ReadFrom(buffer)
@@ -20,20 +20,62 @@ func captureUdpPacket(addr string) {
 	conn.WriteTo([]byte(response), dst)
 }
 
+func udpListen(addr string) {
+	conn, _ := net.ListenPacket("udp", addr)
+	conn.ReadFrom(make([]byte, 1024))
+}
+
 func TestClient_SendPacket(t *testing.T) {
-	addr := ":8000"
+	t.Run("Unknown host", func(t *testing.T) {
+		client := udpclient.New()
+		response, err := client.SendPacket("foo:666", []byte{1, 2, 3})
+		assert.Equal(t, []byte{}, response)
+		assert.ErrorContains(t, err, "dial udp4: lookup foo:")
+	})
 
-	go func() {
-		captureUdpPacket(addr)
-	}()
+	t.Run("Write timeout", func(t *testing.T) {
+		client := udpclient.New()
+		client.Config.TimeoutInMs = 0
+		response, err := client.SendPacket(":8001", []byte("PING"))
 
-	time.Sleep(10 * time.Millisecond)
+		assert.Equal(t, []byte{}, response)
+		assert.ErrorContains(t, err, "write udp4")
+		assert.ErrorContains(t, err, ":8001: i/o timeout")
+	})
 
-	client := udpclient.New()
-	response, err := client.SendPacket(addr, []byte("PING"))
+	t.Run("Read timeout", func(t *testing.T) {
+		addr := ":8002"
 
-	assert.Equal(t, "OK: PING", string(response))
-	assert.Equal(t, nil, err)
+		go func() {
+			udpListen(addr)
+		}()
+
+		time.Sleep(10 * time.Millisecond)
+
+		client := udpclient.New()
+		client.Config.TimeoutInMs = 20
+		response, err := client.SendPacket(":8002", []byte("PING"))
+
+		assert.Equal(t, []byte{}, response)
+		assert.ErrorContains(t, err, "read udp4")
+		assert.ErrorContains(t, err, ":8002: i/o timeout")
+	})
+
+	t.Run("Success", func(t *testing.T) {
+		addr := ":8003"
+
+		go func() {
+			udpListenAndEcho(addr)
+		}()
+
+		time.Sleep(10 * time.Millisecond)
+
+		client := udpclient.New()
+		response, err := client.SendPacket(addr, []byte("PING"))
+
+		assert.Equal(t, "OK: PING", string(response))
+		assert.Equal(t, nil, err)
+	})
 }
 
 func TestClient_SendCommand(t *testing.T) {
@@ -82,7 +124,7 @@ func TestClient_SendCommand(t *testing.T) {
 			udpAddress := fmt.Sprintf(":%d", tc.port)
 
 			go func() {
-				captureUdpPacket(udpAddress)
+				udpListenAndEcho(udpAddress)
 			}()
 
 			time.Sleep(10 * time.Millisecond)
